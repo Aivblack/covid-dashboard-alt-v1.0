31d454798edd89fbfbd8e6dbb73a51f6d0fef731
Adrian <aivillacorta.black@gmail.com>
2021-02-03 15:55:00 +0800

    Final

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
 const population = document.getElementById("population-count");
 const deathsToday = document.getElementById("deaths-today-count");
 const caseToday = document.getElementById("new-cases-today");
+const totalCases = document.getElementById('total-case-count');
+const activeCases = document.getElementById('active-case-count');
+const recoveries = document.getElementById('recoveries-count');
+const deaths = document.getElementById('deaths-count');
 
 fetch('https://corona-api.com/countries')
   .then(response => response.json())
   .then(data => {
       console.log(data);
+
+      for (i=0; i < data.data.length; i++){
+        let targetSelect = document.getElementById("country");
+        let option = document.createElement("option");
+        option.text = data.data[i].name;
+        targetSelect.add(option);
+      }
     
       population.innerText = data.data[231].population.toLocaleString();
       deathsToday.innerText = data.data[231].today.deaths.toLocaleString();
@@ -47,3 +58,22 @@ fetch('https://corona-api.com/countries')
         }
       });
     });
+
+function selectCountry(){
+  fetch('https://corona-api.com/countries')
+    .then(response => response.json())
+    .then(data => {
+
+        let targetIndex = document.getElementById("country").selectedIndex;
+        let index = targetIndex - 1;
+        let targetCountryName = document.getElementById("country-name");
+        let y = document.getElementById("country").options;
+        targetCountryName.innerText = y[targetIndex].text;
+
+        totalCases.innerText = data.data[index].latest_data.confirmed;
+        activeCases.innerText = data.data[index].latest_data.critical;
+        recoveries.innerText = data.data[index].latest_data.recovered;
+        deaths.innerText = data.data[index].latest_data.deaths;
+    });
+}
+
