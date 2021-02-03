db74762b147ca6f7cd83c4e6be1713bbd4e7db38
Adrian <aivillacorta.black@gmail.com>
2021-02-03 15:09:05 +0800

    Added graph

--- script.js
+++ script.js
@@ -13,4 +13,37 @@ fetch('https://corona-api.com/countries')
       let dateSlice = data.data[231].updated_at;
       let dateCut = dateSlice.slice(0, 10);
       document.getElementById('update').innerText = dateCut;
-  });
+
+      // Chart
+      let totalChart = data.data[231].latest_data.confirmed;
+      let activeChart = data.data[231].latest_data.critical;
+      let recoveriesChart = data.data[231].latest_data.recovered;
+      let deathsChart = data.data[231].latest_data.deaths;
+
+      Chart.defaults.global.defaultFontFamily = 'Montserrat';
+
+      let myChart0 = document.getElementById('myChart').getContext('2d');
+      let chart = new Chart(myChart0, {
+        type: 'bar',
+        data: {
+          labels: ['Total cases', 'Active cases', 'Recoveries', 'Deaths'],
+          datasets: [{
+            label: 'Count',
+            data: [
+              totalChart,
+              activeChart,
+              recoveriesChart,
+              deathsChart
+            ],
+            backgroundColor:'#3F3F3F',
+            borderWidth: 4,
+            borderColor: 'black'
+          }]
+        },
+        options: {
+            legend: {
+            display: false
+          }
+        }
+      });
+    });
